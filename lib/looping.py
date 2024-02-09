665acf39c670f97966467a0882bca745fcba53c0
dennis <dennis.mwangi@student.moringaschool.com>
2024-02-09 10:30:29 +0300

    All  tests passed

--- lib/looping.py
+++ lib/looping.py
@@ -3,11 +3,34 @@
 def happy_new_year():
     # code goes here!
     pass
+    i = 10
+    while i > 0 :
+        print(i)
+        i -= 1
+        if i == 0:
+            print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
     pass
+    int_list = [num**2 for num in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
     pass
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(f"{i}")
+        
+
+
+
+print(fizzbuzz())
+
